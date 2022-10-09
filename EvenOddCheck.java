d33a67f9de397ab3c2b5bfb950817c83fa6d2131
CodeWith-HAMZA <115408642+CodeWith-HAMZA@users.noreply.github.com>
2022-10-09 18:44:58 +0500

    Update EvenOddCheck.java

--- EvenOddCheck.java
+++ EvenOddCheck.java
@@ -1,18 +1,27 @@
 import java.util.*;
 
 class EvenOddCheck {
-    public static void main(String[] args) {
+    public static void main(void) {
 
         System.out.println("enter the number");
         Scanner inputFromConsole = new Scanner(System.in);
-        int chechout = inputFromConsole.nextInt();
-        if (chechout % 2 == 0) {
-            System.out.println("number is even");
-
-        } else {
-            System.out.println("number is odd");
-
-        }
-
+        int num = inputFromConsole.nextInt();
+        
+        EvenOddCheck checker = new EvenOddCheck();
+                
+        String result = checker.isEvenOrOdd(num(num);
+        System.out.println(result);
+        
+        
+        return;
+    }
+    
+    
+   String isEvenOrOdd(int givenNumber){
+        return(
+            ((givenNumber / 2) * 2 == givenNumber ) ?
+                "This One Is Even":
+                "This One Is Odd"
+        );
     }
 }
