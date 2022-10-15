a50fb78f17dc212c6e51d19043812ef4712c932c
Muhammad Hamza Jawaid Shaikh <115408642+CodeWith-HAMZA@users.noreply.github.com>
2022-10-15 21:29:17 +0500

    Add files via upload

---
+++ VariablesDataTypes.java
@@ -0,0 +1,20 @@
+/**
+ * Data-Types-Variables
+ */
+public class VariablesDataTypes {
+    public static void main(String[] args) {
+        int myNum = 5; // Integer
+        float myFloatNum = 5.99f; // Floating Point Number
+        double myDoubleNum = 53.99; // Floating Point Number
+        char myLetter = '['; // Character
+        boolean myBool = true; // Boolean
+        String myText = "Hello"; // Strings In Java
+
+        System.out.println("Integer-Variable " + myNum);
+        System.out.println("Float-Variable " + myFloatNum);
+        System.out.println("Double-Variable " + myDoubleNum);
+        System.out.println("Boolean-Variable " + myBool);
+        System.out.println("char-Variable " + myLetter);
+        System.out.println("string-Variable " + myText);
+    }
+};
