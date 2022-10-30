0d48d624d7a3bc0a6655e874e887fa7c6e9813d0
Muhammad Hamza Jawaid Shaikh <115408642+CodeWith-HAMZA@users.noreply.github.com>
2022-10-30 07:03:42 -0700

    Update and rename task7 to Escape_Sequence.java

---
+++ Escape_Sequence.java
@@ -0,0 +1,12 @@
+class Escape_Sequence{
+      public static void main(String[]args){
+            System.out.println("Demo \n This is BackSlash n to Start New-Line ");  
+            System.out.println("Hey there \t BackSlash t is used to add tab spaces   ");        
+            System.out.println("Back Slash r  \r Is Used To Move The Cursor On To The Starting Location Of the Current Line  "); // '\r'   carriage retain
+
+            System.out.println("BackSpace Character (BackSlash b) \b  "); //b for backspace
+            System.out.println("BackSlash Backslash \\ Is Used To Create A Single Backspace  ");  
+            System.out.println(" Backslash double quote is used to add a double code in the string \"Wrapped With Double Quotes\" ");  
+            System.out.println("\'  Backslash single quote is used to add a single quote in the string just like above one");  
+      }
+}
